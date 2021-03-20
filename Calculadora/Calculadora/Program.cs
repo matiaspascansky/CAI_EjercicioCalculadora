2b715e9bab30ae7765f42ea38653358870c0a641
Matias <mpascansky@gmail.com>
2021-03-19 21:40:07 -0300

    trabajo terminado

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -11,15 +11,33 @@ namespace Calculadora
             String mensaje = "Ingrese un número";
 
             int numero1=  Validaciones.ValidacionesNumericas.solicitarNumero(mensaje);
-
+                
             mensaje = "Ingrese otro número";
 
             int numero2 = Validaciones.ValidacionesNumericas.solicitarNumero(mensaje);
 
             string signo = Validaciones.ValidacionesNumericas.solicitarSigno();
 
-            Console.WriteLine("SIGNO = " + signo);
-            
+            int resultado;
+            if (signo == "+")
+            {
+                resultado = Operaciones.OperacionesNumericas.suma(numero1, numero2);
+            } else if (signo == "-")
+            {
+                resultado = Operaciones.OperacionesNumericas.resta(numero1, numero2);
+
+            } else if (signo == "*")
+            {
+                resultado = Operaciones.OperacionesNumericas.multiplicacion(numero1, numero2);
+
+            }
+            else
+            {
+                resultado = Operaciones.OperacionesNumericas.division(numero1, numero2);
+
+            }
+
+            Console.WriteLine("EL RESULTADO ES: " + resultado);
 
 
 
---
+++ Calculadora/Operaciones/OperacionesNumericas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Operaciones
+{
+    public class OperacionesNumericas
+    {
+        public static int suma(int nro1, int nro2)
+        {
+            return nro1 + nro2;
+        }
+
+        public static int resta(int nro1, int nro2)
+        {
+            return nro1-nro2;
+        }
+        public static int multiplicacion(int nro1, int nro2)
+        {
+            return nro1 * nro2;
+        }
+        public static int division(int nro1, int nro2)
+        {
+            int res = 0;
+            if (nro2 != 0)
+            {
+                res =  nro1 / nro2;
+            }
+
+            return res;
+        }
+    }
+}
